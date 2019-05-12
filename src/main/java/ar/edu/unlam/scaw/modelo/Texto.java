ec8156f9ec49ce96e989966b14fbbd0dbf4c5d0b
Federico <federaste14@gmail.com>
2019-05-12 18:22:45 -0300

    Creacion modelos BD

---
+++ src/main/java/ar/edu/unlam/scaw/modelo/Texto.java
@@ -0,0 +1,43 @@
+package ar.edu.unlam.scaw.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Texto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_usuario", referencedColumnName="id")
+	private Usuario usuario;
+
+	private String descripcion;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public void setUsuario(Usuario usuario){
+		this.usuario = usuario;
+	}
+	public Usuario getUsuario(){
+		return usuario;
+	}
+	
+}
