658d9776f3d8717b6eac584a1468e61f865591fe
rrondosa <1017.reyna@gmail.com>
2019-05-12 04:00:34 -0300

    cambios para la conexion del hsql

---
+++ src/main/java/ar/edu/unlam/scaw/persistencia/HsqlDataSource.java
@@ -0,0 +1,28 @@
+package ar.edu.unlam.scaw.persistencia;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(locations = {"/applicationContext.xml"})
+public class HsqlDataSource {
+
+	@Bean
+	public DataSource dataSource() {
+
+		System.out.println("create DDBB !!!!!!!!!");
+
+		// no need shutdown, EmbeddedDatabaseFactoryBean will take care of this
+
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.HSQL).addScript("db/sql/create-db.sql")
+				.addScript("db/sql/insert-data.sql").build();
+		return db;
+		
+	}
+
+}
---
+++ src/main/resources/db/sql/insert-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO person (NOMBRE, APELLIDO) VALUES ('Reyna', 'Rondo'); 
+
+INSERT INTO usuario(EMAIL,PASSWORD,ROL) VALUES  ('user1','123','admin');
+
+INSERT INTO tarea(NOMBRE,DESCRIPCION,ESTADO) VALUES ('ingreso','se logea','1');
+
+
