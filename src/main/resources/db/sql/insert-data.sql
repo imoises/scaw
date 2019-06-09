1510f8987f5450d6ab8dd6ddf2cdb299a1e044b7
Tomás <tomas.1412@gmail.com>
2019-06-09 18:36:49 -0300

    Se elimina sql de preguntas de seguridad

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -48,29 +48,7 @@ INSERT INTO TEXTO (DESCRIPCION,ID_USUARIO) VALUES ('se guarda texto',1);
 ---------------------------------------------------------------------
 INSERT INTO PASSWORD_VIEJO(PASSWORD, FECHA, ID_USUARIO) VALUES ('$2a$10$WW470h3a/SnE/9o3AWarr.zSKDseXv87rN11sQwtmCo.5Z8/oQg2.',timestamp '2017-10-12 21:22:23' ,2);
 ---------------------------------------------------------------------   
---             TABLA PREGUNTAS DE SEGURIDAD                        --
----------------------------------------------------------------------
-INSERT INTO PREGUNTA(PREGUNTA ) VALUES('¿Pregunta 1?');
-INSERT INTO PREGUNTA(PREGUNTA ) VALUES('¿Pregunta 2?');
-INSERT INTO PREGUNTA(PREGUNTA ) VALUES('¿Pregunta 3?');
----------------------------------------------------------------------   
---             TABLA SEGURIDAD                                    --
----------------------------------------------------------------------
-INSERT INTO SEGURIDAD(RESPUESTA, ID_USUARIO) VALUES('Respuesta 1 user 1',1);
-INSERT INTO SEGURIDAD(RESPUESTA, ID_USUARIO) VALUES('Respuesta 2 user 1',1);
-INSERT INTO SEGURIDAD(RESPUESTA, ID_USUARIO) VALUES('Respuesta 3 user 1',1);
-INSERT INTO SEGURIDAD(RESPUESTA, ID_USUARIO) VALUES('Respuesta 1 user 2',2);
-INSERT INTO SEGURIDAD(RESPUESTA, ID_USUARIO) VALUES('Respuesta 2 user 2',2);
-INSERT INTO SEGURIDAD(RESPUESTA, ID_USUARIO) VALUES('Respuesta 3 user 2',2);
----------------------------------------------------------------------   
---             TABLA INTERMEDIA ENTRE SEG-PREG                     --
----------------------------------------------------------------------
-INSERT INTO SEGURIDAD_PREGUNTA(ID_SEGURIDAD,ID_PREGUNTA )	VALUES(1,1);
-INSERT INTO SEGURIDAD_PREGUNTA(ID_SEGURIDAD,ID_PREGUNTA )	VALUES(2,2);
-INSERT INTO SEGURIDAD_PREGUNTA(ID_SEGURIDAD,ID_PREGUNTA )	VALUES(3,3);
-INSERT INTO SEGURIDAD_PREGUNTA(ID_SEGURIDAD,ID_PREGUNTA )	VALUES(4,1);
-INSERT INTO SEGURIDAD_PREGUNTA(ID_SEGURIDAD,ID_PREGUNTA )	VALUES(5,2);
-INSERT INTO SEGURIDAD_PREGUNTA(ID_SEGURIDAD,ID_PREGUNTA )	VALUES(6,3);
+
 
 
 
